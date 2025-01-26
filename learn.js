e08691dec64c1f00de4baaeb3fa954fa92669344
Ashish Kumar Singh <ashishsinghxr@gmail.com>
2025-01-26 19:10:44 +0530

    Add files via upload

---
+++ learn.js
@@ -0,0 +1,11 @@
+// const myNumber = 5;
+// const myString = 'Hello, World!';
+// const myBoolean = true;
+// const myArray = [1, 2, 3];
+// const myObject = { name: 'John', age: 30 };
+// console.log(myNumber, myString, myBoolean, myArray, myObject);
+
+const acountID = 120410491; // const cannot be changed
+let accountName = "John Doe";
+var accountPassword = "ncw9hrn2rh290hr";
+console.log(acountID, accountName, accountPassword);
